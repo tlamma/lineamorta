0bcc62557a65fcf7c6e58ab2ee84471059a25df7
tlamma <tlamma.personal@gmail.com>
2026-01-09 15:29:23 +0100

    lm6

--- script/write.py
+++ script/write.py
@@ -6,15 +6,16 @@ import array
 lm3 = "Previsto il prevedibile || Foreseen the Foreseeable (May 25)"
 lm4 = "Raptus Aviario || Down the goose hole (Jun 25)"
 lm5 = "Il Giorno dei Pareri || Opinion's Day (Oct 25)"
+lm6 = "Ode all'Oblìo || Ode to Oblivion (Dec 25)"
 names = []
 text = []
 eng = []
 partialhtml = []
 
-with open('form_responses.csv', mode ='r')as form:
+with open('form_again.csv', mode ='r')as form:
     csvFile = csv.reader(form)
     for lines in csvFile:
-        if lines[1]==lm5:                            # CAMBIA IL TEMA
+        if lines[1]==lm6:                            # CAMBIA IL TEMA
             names.append(lines[2])
             text.append(lines[3])
             eng.append(lines[4])
@@ -27,7 +28,7 @@ for i in range(0,len(text)):
     rsub = substring.replace("TESTO",text[i].replace("\n","<br>")).replace("TEXT",eng[i].replace("\n","<br>")).replace("PSEUDONIMO",names[i].replace("\n","<br>"))
     partialhtml.append(rsub)
 
-output = open("lm5_internal.html",'w')              # CAMBIA TEMA
+output = open("lm6_internal.html",'w')              # CAMBIA TEMA
 output.write(str(mainfile)+"\n")
 for i in range(0,len(partialhtml)):
     output.write(str(partialhtml[i])+"\n")
