357883a4f64231e6f3efe2a56967ecbd35dfc379
tlamma <tlamma.personal@gmail.com>
2026-03-05 17:08:09 +0100

    lm7

--- script/write.py
+++ script/write.py
@@ -7,15 +7,16 @@ lm3 = "Previsto il prevedibile || Foreseen the Foreseeable (May 25)"
 lm4 = "Raptus Aviario || Down the goose hole (Jun 25)"
 lm5 = "Il Giorno dei Pareri || Opinion's Day (Oct 25)"
 lm6 = "Ode all'Oblìo || Ode to Oblivion (Dec 25)"
+lm7 = "Pagina Nera || Black Page ( Feb 26 )"
 names = []
 text = []
 eng = []
 partialhtml = []
 
-with open('form_again.csv', mode ='r')as form:
+with open('collector2026.csv', mode ='r')as form:
     csvFile = csv.reader(form)
     for lines in csvFile:
-        if lines[1]==lm6:                            # CAMBIA IL TEMA
+        if lines[1]==lm7:                            # CAMBIA IL TEMA
             names.append(lines[2])
             text.append(lines[3])
             eng.append(lines[4])
@@ -28,7 +29,7 @@ for i in range(0,len(text)):
     rsub = substring.replace("TESTO",text[i].replace("\n","<br>")).replace("TEXT",eng[i].replace("\n","<br>")).replace("PSEUDONIMO",names[i].replace("\n","<br>"))
     partialhtml.append(rsub)
 
-output = open("lm6_internal.html",'w')              # CAMBIA TEMA
+output = open("lm7_internal.html",'w')              # CAMBIA TEMA
 output.write(str(mainfile)+"\n")
 for i in range(0,len(partialhtml)):
     output.write(str(partialhtml[i])+"\n")
